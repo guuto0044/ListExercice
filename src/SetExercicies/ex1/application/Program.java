23d01efed91b75f012b893e6f043ee4ed7ae1915
Guto <guuto0044@gmail.com>
2025-05-23 17:03:14 -0300

    Set and BufferedReader

--- src/SetExercicies/ex1/application/Program.java
+++ src/SetExercicies/ex1/application/Program.java
@@ -1,4 +1,47 @@
 package SetExercicies.ex1.application;
 
+import SetExercicies.ex1.entities.User;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Program {
+    public static void main(String[] args) {
+//        String file = "/Users/augustocesarsouza/Documents/exemplo1.txt";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter fille full path: ");
+        String file = sc.next();
+
+        File path = new File(file);
+
+        Set<User> setUsers = new HashSet<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String line = br.readLine();
+
+            while (line != null){
+                String [] fields = line.split(" ");
+                String nickname = fields[0];
+                Date dateTime = Date.from(Instant.parse(fields[1]));
+                User user = new User(nickname,dateTime);
+                setUsers.add(user);
+                line = br.readLine();
+            }
+
+            System.out.print("Total Users:" + setUsers.size());
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
--- src/SetExercicies/ex1/entities/User.java
+++ src/SetExercicies/ex1/entities/User.java
@@ -1,4 +1,47 @@
 package SetExercicies.ex1.entities;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class User {
+
+    private String nickname;
+    private Date dateTime;
+
+    public User(){
+
+    }
+
+    public User(String nickname, Date dateTime) {
+        this.nickname = nickname;
+        this.dateTime = dateTime;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(nickname, user.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nickname);
+    }
 }
